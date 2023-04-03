f8073cb1aa51ea14bd4385ee7bfa910fd98e3133
Yuri <yuryanurov@gmail.com>
2023-04-03 17:33:59 +0600

    user created

--- blog/schemas.py
+++ blog/schemas.py
@@ -4,3 +4,17 @@ from pydantic import BaseModel
 class Blog(BaseModel):
     title: str
     body: str
+
+
+class ShowBlog(Blog):
+    class Config():
+        orm_mode = True
+"""
+class ShowBlog(BaseModel):
+    title: str
+    body: str"""
+
+class User(BaseModel):
+    name:str
+    email:str
+    password: str
